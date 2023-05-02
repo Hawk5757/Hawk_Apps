d04cbd9719e1d78fa133d59283fd4cab9f0d3626
Artem Herasymenko <artemka5757@gmail.com>
2023-05-03 01:34:24 +0300

    First

---
+++ EFDataAccessLibrary/Models/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace EFDataAccessLibrary.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ EFDataAccessLibrary/Models/Recipe.cs
@@ -0,0 +1,11 @@
+﻿namespace EFDataAccessLibrary.Models;
+
+public class Recipe
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Ingredient { get; set; }
+    public string StepByStep { get; set; }
+    public string Kind { get; set; }
+    public string FoodClassification { get; set; }
+}
